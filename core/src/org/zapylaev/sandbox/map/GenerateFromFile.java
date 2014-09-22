268736d62da442ac7508a3dd40f7568318e370d7
k.zapylaev <k.zapylaev@gmail.com>
2014-09-22 23:19:25 +0300

    increase test map

--- core/src/org/zapylaev/sandbox/map/GenerateFromFile.java
+++ core/src/org/zapylaev/sandbox/map/GenerateFromFile.java
@@ -11,11 +11,10 @@ public class GenerateFromFile implements MapGenerator {
 
     @Override
     public int[][] generateMap(int size) {
-        if (size != 10) throw new IllegalArgumentException("must be 10");
         Scanner mapScanner = new Scanner(Gdx.files.internal("sample_map").read());
-        int[][] mapBuffer = new int[10][10];
-        for (int i = 0; i < 10; i++) {
-            for (int j = 0; j < 10; j++) {
+        int[][] mapBuffer = new int[size][size];
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size; j++) {
                 mapBuffer[i][j] = mapScanner.nextInt();
             }
         }
